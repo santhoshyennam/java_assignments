4215333a61b7034e61629e06a893e7c289948355
santhosh yennam <sandysanthosh045@gmail.com>
2021-05-05 20:31:51 +0530

    course 2

--- coursera/course_2/CaesarCipher.java
+++ coursera/course_2/CaesarCipher.java
@@ -1,9 +1,14 @@
 package coursera.course_2;
 
- class Caesar{
+public class CaesarCipher {
     
-   
+    static String encrypt(String input,int key)
+    {
+       String alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+       String encrypted_alphabet = alphabet.substring(key)+alphabet.substring(0,key);
+       return encrypted_alphabet;
+    }
     public static void main(String[] args) {
-       // System.out.println(encrypt("df", 22));
+        System.out.println(encrypt("df", 22));
     }
 }
